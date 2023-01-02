7a48e6460627cd64da05039c796bcf7f5afe3644
lianschedule@gmail.com <lianschedule@gmail.com>
2023-01-03 02:40:53 +0800

    去掉未使用的use

--- demo/MyProject/index.php
+++ demo/MyProject/index.php
@@ -2,7 +2,6 @@
 include_once __DIR__.DIRECTORY_SEPARATOR.'packages'.DIRECTORY_SEPARATOR.'pack';
 use A\A;
 use B\B;
-use time\Time;
 //分别调用A()和B()并查看结果
 new A();//A v4.0Time v1.0
 new B();//B v6.3Time v2.0
